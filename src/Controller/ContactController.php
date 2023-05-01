75a512b5597dbf3e2476dee4bcc51960e333b9ba
Romain Touchet <romain.touchet@gmail.com>
2023-05-01 19:09:09 +0200

    contact form display

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,17 +2,35 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'contact_form')]
-    public function index(): Response
+    public function contact(Request $request): Response
     {
-        return $this->render('contact/form.html.twig', [
-            'controller_name' => 'ContactController',
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $email = (new Email())
+                ->from($request->request)
+                ->to('contact@the_boutique.com')
+                ->subject('test')
+                ->text('test');
+
+
+            $this->mailer->send($email);
+        }
+
+
+
+        return $this->renderForm('contact/form.html.twig', [
+            'form' => $form,
         ]);
     }
 }
--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -17,13 +18,33 @@ class ContactType extends AbstractType
             ->add('email', EmailType::class, [
                 'required' => true,
                 'attr' => [
-                    'placeholder' => 'Veuillez renseigner votre adresse email.'
+                    'placeholder' => 'Veuillez renseigner votre adresse email.',
+                    'class' => 'bg-secondary-subtle m-1 ps-1 pe-3 w-100'
                 ]
             ])
-            ->add('objet', TextType::class)
-            ->add('message', TextType::class)
-            ->add('Envoyer', SubmitType::class)
-        ;
+            ->add(
+                'objet',
+                TextType::class,
+                [
+                    'required' => true,
+                    'attr' => [
+                        'placeholder' => 'Objet du contact',
+                        'class' => 'bg-secondary-subtle m-1 ps-1 pe-3 w-100'
+                    ]
+                ]
+            )
+            ->add(
+                'message',
+                TextareaType::class,
+                [
+                    'required' => true,
+                    'attr' => [
+                        'placeholder' => 'Votre message',
+                        'class' => 'bg-secondary-subtle m-1 ps-1 pe-3 w-100'
+                    ]
+                ]
+            )
+            ->add('Envoyer', SubmitType::class, ['attr' => ['class' => 'bg-success-subtle ms-5']]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
