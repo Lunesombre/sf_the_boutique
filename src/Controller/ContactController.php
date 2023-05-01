40ec2084966242a49bed00b32a9fb1590aba6518
Romain Touchet <romain.touchet@gmail.com>
2023-05-01 19:40:08 +0200

    contact form working, with flash message

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -6,25 +6,31 @@ use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'contact_form')]
-    public function contact(Request $request): Response
+    public function contact(Request $request, MailerInterface $mailer): Response
     {
         $form = $this->createForm(ContactType::class);
         $form->handleRequest($request);
+
         if ($form->isSubmitted() && $form->isValid()) {
+            $contactFormData = $form->getData();
             $email = (new Email())
-                ->from($request->request)
+                ->from($contactFormData['email'])
                 ->to('contact@the_boutique.com')
-                ->subject('test')
-                ->text('test');
+                ->subject($contactFormData['objet'])
+                ->text($contactFormData['message']);
 
+            $mailer->send($email);
 
-            $this->mailer->send($email);
+            $this->addFlash('success', 'Demande de contact envoyée.');
+
+            return $this->redirectToRoute('app_index');
         }
 
 
